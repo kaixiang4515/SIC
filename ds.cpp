88f53ba8cf6b48fbe647d59494773a757cb22bf8
Kevin <kaixiang4515@gmail.com>
2023-11-19 16:38:17 +0800

    hash table updated

--- ds.cpp
+++ ds.cpp
@@ -4,19 +4,25 @@
 hash_table::hash_table() {
     cnt = 0;
     for(int i = 0; i < 1500; ++i) {
-        arr[i] = "";
+        arr[i].value = "";
     }
 }
 
+unsigned int hash_table::hash(std::string key) {
+    return (unsigned int)(int(std::hash<std::string>{}(key))%691+700);
+}
+
 void hash_table::insert(std::string key, std::string value) {
-    int t = int(std::hash<std::string>{}(key))%691+700;
-    arr[t] = value;
+    arr[hash(key)].value = value;
     ++cnt;
 }
 
+bool hash_table::find(std::string key) {
+    return arr[hash(key)].value != "";
+}
+
 std::string hash_table::at(std::string key) {
-    int t = int(std::hash<std::string>{}(key))%691+700;
-    return arr[t];
+    return arr[hash(key)].value;
 }
 
 unsigned int hash_table::size() {
--- ds.h
+++ ds.h
@@ -1,14 +1,19 @@
 #ifndef DS_H
 #define DS_H
 #include <string>
+struct table{
+    std::string value;
+};
 class hash_table {
 public:
     hash_table();
+    unsigned int hash(std::string key);
     void insert(std::string key, std::string value);
+    bool find(std::string key);
     std::string at(std::string key);
     unsigned int size();
 private:
-    std::string arr[1500];
+    table arr[1500];
     unsigned int cnt;
 };
 #endif // DS_H
