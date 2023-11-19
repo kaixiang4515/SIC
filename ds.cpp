0fe0d161563a20dd103abccb2bd8f9d310df70a4
Kevin <kaixiang4515@gmail.com>
2023-11-19 19:44:33 +0800

    add BST for SYMTAB

--- ds.cpp
+++ ds.cpp
@@ -1,11 +1,13 @@
 #include "ds.h"
 #include <functional>
 #include <iostream>
+
+bool SYM_table::operator<(const SYM_table& b) const {
+    return this->label < b.label;
+}
+
 hash_table::hash_table() {
     cnt = 0;
-    for(int i = 0; i < 1500; ++i) {
-        arr[i].value = "";
-    }
 }
 
 unsigned int hash_table::hash(std::string key) {
@@ -26,5 +28,53 @@ std::string hash_table::at(std::string key) {
 }
 
 unsigned int hash_table::size() {
+    return cnt;
+}
+
+BST::BST() {
+    cnt = 0;
+    last = 1;
+}
+
+void BST::insert(std::string label, unsigned int address) {
+    unsigned int index = 1;
+    if (last == 1) {
+        arr[last].label = label;
+        arr[last].address = address;
+        ++cnt;
+        ++last;
+        return;
+    }
+
+    while (index < last && arr[index].label != "") {
+        if (label < arr[index].label) {
+            index = index * 2;
+        } else {
+            index = index * 2 + 1;
+        }
+    }
+    arr[index].label = label;
+    arr[index].address = address;
+    ++cnt;
+    if (index >= last) {
+        last = index + 1;
+    }
+}
+
+unsigned int BST::find(std::string label) {
+    unsigned int index = 1;
+    while (index < last) {
+        if (label  == arr[index].label) {
+            return index;
+        } else if (label < arr[index].label) {
+            index = index * 2;
+        } else {
+            index = index * 2 + 1;
+        }
+    }
+    return 0;
+}
+
+unsigned int BST::size() {
     return cnt;
 }
--- ds.h
+++ ds.h
@@ -1,9 +1,19 @@
 #ifndef DS_H
 #define DS_H
 #include <string>
-struct table{
+
+struct OP_table{
     std::string value;
 };
+
+struct SYM_table{
+    std::string label;
+    unsigned int address = 0;
+    unsigned int length = 0;
+    bool err_flag = false;
+    bool operator<(const SYM_table& b) const;
+};
+
 class hash_table {
 public:
     hash_table();
@@ -13,7 +23,20 @@ public:
     std::string at(std::string key);
     unsigned int size();
 private:
-    table arr[1500];
+    OP_table arr[1500];
     unsigned int cnt;
 };
+
+class BST {
+public:
+    BST();
+    void insert(std::string label, unsigned int address);
+    unsigned int find(std::string lebel); // return index of the array
+    unsigned int size();
+private:
+    SYM_table arr[1500];
+    unsigned int cnt;
+    unsigned int last;
+};
+
 #endif // DS_H
